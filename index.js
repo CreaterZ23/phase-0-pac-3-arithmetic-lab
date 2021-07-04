5c1beca2da0912bc92f9ea361bf5b005e4a7043f
Zeon <zeonstewart23@gmail.com>
2021-07-04 10:09:19 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,49 @@
+function add() {
+    return 3 + 3;
+}
+
+function subtract() {
+    return 2 - 1;
+}
+
+function multiply() {
+    return 3 * 2;
+}
+
+function divide() {
+    return 3 / 1;
+} 
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function increment(n) {
+    n++;
+    return n;
+}
+
+function decrement(n) {
+    n--;
+    return n;
+}
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
